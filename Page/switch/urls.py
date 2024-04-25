8d200d861d43f7d54dbfc5d13899c195388a8c3d
ElcinQuluyev <elcinq743@gmail.com>
2024-04-25 17:40:07 +0400

    add error olub elcin

---
+++ Page/switch/urls.py
@@ -0,0 +1,30 @@
+from django.urls import path
+from .views import *
+"""
+#- Bu 2 ferqli url ucun isdifade olunmasi 
+
+#- daha doqrusu /users/create/
+# ve /advertisement/create/
+
+
+
+"""
+app_name = "switch" 
+
+urlpatterns = [
+    path(
+        '/home/',
+        home,
+        name="home" 
+    ),
+    path(
+        '/card-details',
+        card_details,
+        name="card-details" 
+    ),
+    # path(
+    #     'create/',
+    #     home,
+    #     name="create" 
+    # ),
+]
--- Page/switch/views.py
+++ Page/switch/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, '/home.html')
+
+def card_details(request):
+    return render(request, 'switch/card_details.html')
