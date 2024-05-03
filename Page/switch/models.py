16797f71df7cbe59130cefcd7b112c831872e42b
ElcinQuluyev <elcinq743@gmail.com>
2024-05-03 15:41:10 +0400

    add: imageField

--- Page/switch/models.py
+++ Page/switch/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Card(models.Model):
     # card_pk = models.ForeignKey(  ,models.CASCADE)
     img_url = models.URLField(max_length=200,null=True, blank=True)
+    image = models.ImageField(upload_to='photos/%Y/%m/%d/', null=True)
     title = models.CharField(max_length=16,null=True, blank=True)
     descriptions = models.CharField(max_length=64, null=True,blank=True)
 
