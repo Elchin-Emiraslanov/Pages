75a717a127b44cf66fdc78a859160534c8d4db70
ElcinQuluyev <elcinq743@gmail.com>
2024-05-03 12:00:28 +0400

    add: view create Card.object.all in show browser

--- Page/switch/views.py
+++ Page/switch/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-
+from .models import Card
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'switch/home.html')
+    cards = Card.objects.all()
+    ctx = {  # context
+        "cards":cards
+    }
+    return render(request, 'switch/home.html', ctx)
+
 
 def card_details(request):
     return render(request, 'switch/card_details.html')
+
+
+
+
