ad2fd83952f9ff2388206018262311f6fa7afbaa
ElcinQuluyev <elcinq743@gmail.com>
2024-05-03 11:58:06 +0400

    add: create Card models {switch/models.py}

--- Page/switch/models.py
+++ Page/switch/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Card(models.Model):
+    # card_pk = models.ForeignKey(  ,models.CASCADE)
+    img_url = models.URLField(max_length=200,null=True, blank=True)
+    title = models.CharField(max_length=16,null=True, blank=True)
+    descriptions = models.CharField(max_length=64, null=True,blank=True)
+
+
+    def __str__(self) -> str:
+        return f"{self.title}"
+
